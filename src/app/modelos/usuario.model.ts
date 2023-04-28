91b31bdb71c03012592da30e93ff8998526e843a
ringgo5 <vicentem424@gmail.com>
2023-04-28 13:31:58 +0200

    first commit

---
+++ src/app/modelos/usuario.model.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+constructor(
+    public id: number = 0,
+    public email: string| undefined,
+    public nombre: string | undefined,
+    public apellido: string | undefined
+){
+    
+}
+}
