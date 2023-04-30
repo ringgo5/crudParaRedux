b618b17929e48a45da11e9eea17c11fbd581ab52
ringgo5 <vicentem424@gmail.com>
2023-04-30 17:18:59 +0200

    Falta Edicion

--- src/app/todos/todo-add/todo-add.component.ts
+++ src/app/todos/todo-add/todo-add.component.ts
@@ -1,36 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app.reducer';
 import { crearTodo } from '../todo.actions';
+//import { Usuario } from 'src/app/modelos/usuario.model';
 
 @Component({
   selector: 'app-todo-add',
   templateUrl: './todo-add.component.html',
   styleUrls: ['./todo-add.component.css']
 })
-export class TodoAddComponent implements OnInit{
+export class TodoAddComponent implements OnInit {
+
+  formularioDatos!: FormGroup;
+  usuario:any;
   
-    formularioDatos!:FormControl;
 
-    constructor(private store:Store<AppState>,
-                private formbuilder: FormBuilder){
-      
-    }
-  ngOnInit(): void {}
-  agregar(){
-    this.formularioDatos=new FormControl(
-      {
+  constructor(private store: Store<AppState>,
+              private formbuilder: FormBuilder) {
+             
+          this.formularioDatos = this.formbuilder.group(
 
-        
-        email: ["",Validators.email],
-        nombre: ["",Validators.pattern('^[A-Z][a-z]*$')], //una validacion para que la primera sea mayúscula
-        apellido: ["",Validators.pattern('^[A-Z][a-z]*$')]
-      }
-    )
-    if(this.formularioDatos.invalid){return;}
-    this.store.dispatch(crearTodo({nombre: this.formularioDatos.value, apellido: this.formularioDatos.value, email: this.formularioDatos.value}))
-    this.formularioDatos.reset();
+            {
+              nombre: ["", [Validators.pattern('^[A-Z][a-z]*$'),Validators.required]],
+              apellido: ["", [Validators.pattern('^[A-Z][a-z]*$'),Validators.required]], //una validacion para que la primera sea mayúscula
+              email: ["", [Validators.email,Validators.required]],
+            }
+          )
+
+  }
+  ngOnInit() {
+    
+
+  }
+  agregar() {
+
+    if (this.formularioDatos?.invalid) { return; }
+    this.usuario = this.formularioDatos.value
+    
+    //console.log(this.formularioDatos?.value)
+    console.log(this.usuario)
+    this.store.dispatch(crearTodo(this.usuario))
+    this.formularioDatos?.reset();
   }
 
 }
--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Usuario } from 'src/app/modelos/usuario.model';
+
 
 @Component({
   selector: 'app-todo-item',
@@ -8,9 +9,30 @@ import { Usuario } from 'src/app/modelos/usuario.model';
 })
 export class TodoItemComponent implements OnInit{
 
+  editando:boolean=false;
+  usuarioEdit:Usuario | undefined;
+ 
+
+
   @Input() todo:Usuario | undefined;
+  @ViewChild('inputFisico') inputFisico?:ElementRef;
+  formularioDatos: any;
   ngOnInit(): void {
     
+  }
+  editar(todo:any){
+      
+      this.editando=true
+      
+      this.inputFisico?.nativeElement.select();
+      this.usuarioEdit=todo
+      console.log(this.usuarioEdit)
+ 
+
+
+  }
+  terminarEdicion(){
+    this.editando=false;
   }
 
 }
